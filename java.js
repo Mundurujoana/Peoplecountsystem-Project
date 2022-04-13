457067f0139ad8e4ee1049f77d616894cfa1e1f7
Mundurujoana <mundujoana@gmail.com>
2022-04-13 14:48:55 +0300

    commit countApp

---
+++ java.js
@@ -0,0 +1,14 @@
+
+
+let counte = 0
+
+function increment(){
+    //use plus eqaul for count
+//Change this to use the plus equal  technique you have learned
+    counte += 1
+    // countE.innerText = counte
+    console.log(counte)
+    //pass in argumnets
+    document.getElementById("counte").innerText= counte
+}
+
